7e198c152c334a4ab4b71d1bf8fcecfab524561f
Adi Tiya Wiranda <aditwiran19@gmail.com>
2025-06-23 15:50:50 +0700

    feat : progres 23 juni

--- frontend-v2/src/pages/module/pelaksana/lembar-kerja.vue
+++ frontend-v2/src/pages/module/pelaksana/lembar-kerja.vue
@@ -166,6 +166,19 @@
               </div>
               <div class="column is-3 mt-5-min">
                 <div class="column is-12">
+                  <span>Suhu </span>      
+                </div>
+                <VField addons>
+                  <VControl>
+                    <VInput v-model="item.suhu" placeholder="Suhu" />
+                  </VControl>
+                  <VControl class="field-addon-body">
+                    <VButton static>°C</VButton>
+                  </VControl>
+                </VField>
+              </div>
+              <div class="column is-3 mt-5-min">
+                <div class="column is-12">
                   <span>Kelembaban Relatif </span>
                 </div>
                 <VField addons>
